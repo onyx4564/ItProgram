981921ede0b66f1b364a52a0c0a015359f9274ab
ALEXANDRO ROXAS <94739163+onyx4564@users.noreply.github.com>
2022-05-18 22:24:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <title>ItProgram</title>
+    </head>
+    <body>
+<header>
+    <?php 
+    require 'blocs/header.php';
+    ?>
+
+</header>
+ 
+ <main>
+   
+    <?php 
+    require 'blocs/main.php';
+    ?>
+
+ </main>
+
+<footer>
+  <?php 
+    require 'blocs/footer.php';
+  ?>
+    
+  </footer>
+  
+    </body>
+</html>
